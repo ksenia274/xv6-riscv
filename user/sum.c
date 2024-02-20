f64d015951b809e4ff8c599f429e497b28d4f846
ksenia274 <st107490@student.spbu.ru>
2024-02-20 22:14:11 +0300

    1b complete

--- user/sum.c
+++ user/sum.c
@@ -4,44 +4,53 @@
 int
 main(int argc, char *argv[])
 {
+
     char input[100];
     char *token1, *token2;
     int num1, num2;
 
-        if (!gets(input, sizeof(input))) {
-            fprintf(2, "Ошибка чтения ввода.\n");
+    char *input_ptr = input;
+    for (int i = 0; i < 100; i++) {
+        if (read(0, input_ptr, 1) != 1) {
+            fprintf(2, "Ошибка чтения файла.\n");
             exit(1);
         }
 
-        if (strchr(input, ' ') == input){
-            token1 = strchr(input, ' ');
-            token1++;
-        }
-        else {
-            token1 = input;
-        }
-        if (!token1 ||( *token1 == '\n')) {
-            fprintf(2, "Некорректный формат данных.\n");
-            exit(1);
-        }
+        if (*input_ptr == '\0' || *input_ptr == '\n' )
+            break;
 
-        num1 = atoi(token1);
+        input_ptr++;
+    }
 
-        token2 = strchr(token1, ' ');
-        token2++;
-        if (!token2 ||( *token2 == '\n')) {
-            fprintf(2, "Некорректный формат данных. Отсутствует второе число.\n");
-            exit(1);
-        }
+    if (strchr(input, ' ') == input){
+        token1 = strchr(input, ' ');
+        token1++;
+    }
+    else {
+        token1 = input;
+    }
+    if (!token1 ||( *token1 == '\n')) {
+        fprintf(2, "Некорректный формат данных.\n");
+        exit(1);
+    }
+
+    num1 = atoi(token1);
+
+    token2 = strchr(token1, ' ');
+    token2++;
+    if (!token2 ||( *token2 == '\n')) {
+        fprintf(2, "Некорректный формат данных. Отсутствует второе число.\n");
+        exit(1);
+    }
 
 
-        num2 = atoi(token2);
+    num2 = atoi(token2);
     if (!num1 || !num2) {
         fprintf(2, "Некорректный формат данных.\n");
         exit(1);
     }
-        int sum =  num1 + num2;
-        printf("%d\n", sum);
+    int sum =  num1 + num2;
+    printf("%d\n", sum);
 
     exit(0);
 }
